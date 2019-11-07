5bf5d0c548bbf1bc26e4fa160d66c9493033becf
Magan Josue <majosue@ga-n1.21-school.ru>
2019-11-07 21:36:48 +0300

    remove cycle from compare

--- newsubset.c
+++ newsubset.c
@@ -6,7 +6,7 @@
 /*   By: majosue <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 12:36:26 by majosue           #+#    #+#             */
-/*   Updated: 2019/10/29 12:43:45 by majosue          ###   ########.fr       */
+/*   Updated: 2019/11/07 21:34:55 by majosue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 
 int		compare(int n, int m, int **subsets)
 {
-	int a;
 	int b;
 
-	a = 1;
 	b = 1;
 	if (subsets[n][0] == subsets[m][0])
 		return (1);
 	while (b < 5)
 	{
-		while (a < 5)
-		{
-			if (subsets[n][b] == subsets[m][a])
-				return (1);
-			a++;
-		}
-		a = 1;
+		if (subsets[n][b] == subsets[m][1] ||\
+				subsets[n][b] == subsets[m][2] ||\
+				subsets[n][b] == subsets[m][3] ||\
+				subsets[n][b] == subsets[m][4])
+			return (1);
 		b++;
 	}
 	return (0);
